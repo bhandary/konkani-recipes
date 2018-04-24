0285b2af5d74551fbcfbe45620aee1b9c9343f8c
Manju Bhandary <bhandary@yahoo.com>
2018-04-23 21:34:02 -0700

    Initial Commit

---
+++ Konkani Recipes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Konkani Recipes
+//
+//  Created by Manju Bhandary on 4/23/18.
+//  Copyright © 2018 Manju Bhandary. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
